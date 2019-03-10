dd477657a9593d7890c66f649c78f5b9e63aff24
Barry Tam <barrytam20@gmail.com>
2019-03-10 15:41:56 -0700

    adding weather to API, dockerizing it

--- hello-api/main.go
+++ hello-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,7 +12,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", sayHello).Methods("GET")
-	r.HandleFunc("/weather/{", getWeather).Methods("GET")
+	r.HandleFunc("/weather/{zip:[0-9]{5}}", getWeather).Methods("GET")
 	http.ListenAndServe(":5000", r)
 }
 
@@ -33,4 +34,72 @@ type Greeting struct {
 	Message string `json:"greeting"`
 }
 
-//https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&zipcode=98101&oneobservation=true&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg
+func getWeather(w http.ResponseWriter, r *http.Request) {
+	zip := mux.Vars(r)["zip"]
+	url := fmt.Sprintf("https://weather.cit.api.here.com/weather/1.0/report.json?product=observation&zipcode=%s&oneobservation=true&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg",
+		zip)
+	response, err := http.Get(url)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(&APIError{
+			Message: err.Error(),
+		})
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	weatherModel := &CITWeather{}
+	err = json.Unmarshal(data, &weatherModel)
+	if weatherModel.Type == "Invalid Request" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(&APIError{
+			Message: fmt.Sprintf("weather for %s not found", zip),
+		})
+	} else {
+		res := &Forecast{
+			Description: weatherModel.Observations.Locations[0].Forecast[0].Description,
+			Icon:        weatherModel.Observations.Locations[0].Forecast[0].Icon,
+			Country:     weatherModel.Observations.Locations[0].Country,
+			State:       weatherModel.Observations.Locations[0].State,
+			City:        weatherModel.Observations.Locations[0].City,
+		}
+		json.NewEncoder(w).Encode(res)
+	}
+}
+
+// CITWeather model for weather response from CIT
+type CITWeather struct {
+	Observations CITObservations `json:"observations"`
+	Type         string          `json:"Type"`
+}
+
+// CITObservations model for weather response from CIT
+type CITObservations struct {
+	Locations []CITLocation `json:"location"`
+}
+
+// CITLocation model for weather response from CIT
+type CITLocation struct {
+	Forecast []CITForecast `json:"observation"`
+	Country  string        `json:"country"`
+	State    string        `json:"state"`
+	City     string        `json:"city"`
+}
+
+// CITForecast model for weather response from CIT
+type CITForecast struct {
+	Description string `json:"description"`
+	Icon        string `json:"iconLink"`
+}
+
+// Forecast is the model for api response
+type Forecast struct {
+	Description string `json:"description"`
+	Icon        string `json:"iconLink"`
+	Country     string `json:"country"`
+	State       string `json:"state"`
+	City        string `json:"city"`
+}
+
+// APIError is a model for api errors
+type APIError struct {
+	Message string `json:"message"`
+}
